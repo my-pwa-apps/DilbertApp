5c891af2b02c2e5fa708304a24e567ed7e58ea1d
Garfield App <garfieldapp@outlook.com>
2023-01-12 08:38:52 +0100

    cleanup

--- app.js
+++ app.js
@@ -174,10 +174,7 @@ function showComic()
   
   fetch(siteUrl, {
     method: "GET",
-   /* headers: {
-      "x-cors-grida-api-key": "77a0175b-4435-49b0-ad18-52d2dea5a548"
-    }*/
-  })
+     })
      .then(function(response) 
      {
       response.text().then(function(text) 
